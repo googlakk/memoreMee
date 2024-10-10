6281264759a0bec219a98a863594ac3df030213c
googlakk <googl.akk@mail.ru>
2024-10-10 12:27:29 +0600

    updated anzan game

--- frontend/src/pages/games/arifmetika/index.tsx
+++ frontend/src/pages/games/arifmetika/index.tsx
@@ -12,18 +12,14 @@ import { withMainLayout } from "@app/hocs/withMainLayout";
 const Arifmetika: FC = () => {
   return (
     <div className="w-full min-h-[calc(100vh)]   bg-anzanMenuBg bg-cover bg-no-repeat bg-center flex flex-col items-center justify-center  ">
-      <div className=" absolute  w-72 desktops:top-44 sLaptop:top-36">
+      <div className=" absolute  w-72 desktops:top-44 sLaptop:top-24">
         <img src={logo} />
       </div>
       <div className=" flex flex-col items-center mt-10 w-full ">
-        <HomePageCard width="260" imgGame={anzan} routes={ROUTES.ANZAN} />
-        <HomePageCard width="260" imgGame={multiply} routes={ROUTES.MULTIPLY} />
-        <HomePageCard
-          width="260"
-          imgGame={generator}
-          routes={ROUTES.MULTIPLY}
-        />
-        <HomePageCard width="260" imgGame={collums} routes={ROUTES.MULTIPLY} />
+        <HomePageCard width="282" imgGame={anzan} routes={ROUTES.ANZAN} />
+        <HomePageCard width="282" imgGame={multiply} routes={ROUTES.MULTIPLY} />
+        <HomePageCard width="282" imgGame={generator} routes={ROUTES.MULTIPLY} />
+        <HomePageCard width="282" imgGame={collums} routes={ROUTES.MULTIPLY} />
       </div>
     </div>
   );
